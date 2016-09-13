c66a23dffad994b004e8639d8b51832d8755e16e
chris48s <c.mshaw@yahoo.co.uk>
2016-09-13 20:28:47 +0100

    update to match new base scraper format

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-from arcgis_scraper import scrape
+from dc_base_scrapers.arcgis_scraper import ArcGisScraper
 
 
 stations_url = "https://maps.bristol.gov.uk/arcgis/rest/services/ext/localinfo/MapServer/15/query?where=OBJECTID+LIKE+%27%25%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson"
@@ -6,5 +6,7 @@ districts_url = "https://maps.bristol.gov.uk/arcgis/rest/services/ext/localinfo/
 council_id = 'E06000023'
 
 
-scrape(stations_url, council_id, 'utf-8', 'stations')
-scrape(districts_url, council_id, 'utf-8', 'districts')
+stations_scraper = ArcGisScraper(stations_url, council_id, 'utf-8', 'stations')
+stations_scraper.scrape()
+districts_scraper = ArcGisScraper(districts_url, council_id, 'utf-8', 'districts')
+districts_scraper.scrape()
