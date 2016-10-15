e9aa74056dafaf2e7dea2792bcd602e61c799ab1
chris48s <c.mshaw@yahoo.co.uk>
2016-10-15 18:28:32 +0100

    Update disricts url
    cool URLs don't change :(

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ from dc_base_scrapers.arcgis_scraper import ArcGisScraper
 
 
 stations_url = "https://maps.bristol.gov.uk/arcgis/rest/services/ext/localinfo/MapServer/16/query?where=OBJECTID+LIKE+%27%25%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson"
-districts_url = "https://maps.bristol.gov.uk/arcgis/rest/services/ext/localinfo/MapServer/135/query?where=OBJECTID+LIKE+%27%25%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson"
+districts_url = "https://maps.bristol.gov.uk/arcgis/rest/services/ext/localinfo/MapServer/134/query?where=OBJECTID+LIKE+%27%25%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson"
 council_id = 'E06000023'
 
 
